cd1608dbd1985bbf412ac29262e55e993bae1935
yakshith05 <yakshith240@gmail.com>
2026-03-15 14:20:34 +0530

    Fronted changes done

--- frontend/constants/api.js
+++ frontend/constants/api.js
@@ -5,10 +5,8 @@ const DEFAULT_PORT = '3000';
 
 let host = DEFAULT_HOST;
 if (Platform.OS === 'android') {
-  // Android emulators map host machine's 127.0.0.1 to 10.0.2.2
-  host = process.env.API_HOST || '10.0.2.2';
+  host ="https://bookapp-qkkt.onrender.com"
 }
 
 export const API_URL = `http://${host}:${DEFAULT_PORT}`;
 
-// If you need to override host for a physical device, set API_HOST env var in dev or replace constants/api.js
